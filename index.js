fde3e44cc47ae89c424affa512536314ae45c49e
Razibul Ahmed <razibul.ahmed@gmail.com>
2018-04-19 03:13:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,14 +16,4 @@ function johnLennonFacts(arrayJLFacts)  {
   return arrayWithExclamation;
 }
 
-const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
 
-var testFacts = johnLennonFacts(facts);
-for (index = 0; index < testFacts.length; index++) {
-  console.log(testFacts[index]);
-}
